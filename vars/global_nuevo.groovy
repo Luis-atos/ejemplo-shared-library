85b3ec7e1e3a2827f8e1c48552b04f6cc41ca217
Luis-atos <104129252+Luis-atos@users.noreply.github.com>
2022-04-25 15:57:41 +0200

    Create global_nuevo.groovy

---
+++ vars/global_nuevo.groovy
@@ -0,0 +1,20 @@
+//global.groovy
+
+import org.cl.*
+
+def call(String param1, String param2){
+
+    println 'Ejecución de Pipeline'
+
+    
+    println 'Inicio'
+    println 'String 1: ' + param1
+     println 'String 2: ' + param2
+
+     println 'Union de 2 Strings: ' 
+     println 'Nombre obtenido desde Json: ' 
+                            
+
+}
+
+return this;
